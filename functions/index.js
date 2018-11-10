0ba08361cddd256849d64832148b19825df6f7c1
Federica <federica.comuni@gmail.com>
2018-11-10 18:57:21 +0100

    Added gateway endpoint function

--- functions/index.js
+++ functions/index.js
@@ -87,7 +87,7 @@ const turnOnDevice = (res, req) => {
   return database.collection('Devices').doc(req.query.id).update({'enabled': true})
       .then(res.status(200).json({
         // here we shd get confirmation from the gateway first, but this is for testing purposes 
-        message: 'Device is On'
+          [req.query.id] : 'Device is On'
     }))
       .catch((err) => {
           console.log('Error getting documents', err);
@@ -109,8 +109,9 @@ exports.turnOnDevice = functions.https.onRequest((req, res) => {
 const turnOffDevice = (res, req) => {
   return database.collection('Devices').doc(req.query.id).update({'enabled': false})
       .then(res.status(200).json({
+          //we shd check if the key exists before sending the rest
         // here we shd get confirmation from the gateway first, but this is for testing purposes 
-        message: 'Device is Off'
+          [req.query.id]: 'Device is Off'
     }))
       .catch((err) => {
           console.log('Error getting documents', err);
@@ -127,4 +128,68 @@ exports.turnOffDevice = functions.https.onRequest((req, res) => {
       };
       turnOffDevice(res,req);
   });
+});
+
+const updateDeviceStatus = (res, req) => {
+    return database.collection('Devices').doc(req.query.id).update({'enabled': req.query.enabled})
+        .then(res.status(200).json({
+            message: [req.query.enabled]
+        }))
+        .catch((err) => {
+                console.log('Error updating database', err);
+            }
+        );
+};
+
+/**
+ * Send a POST request such as
+ * https://us-central1-smarthome-3c6b9.cloudfunctions.net/updateDeviceStatus?id=my9iXu6WvEgx5oNLLegs&enabled=true
+ * to confirm that the device is turned on, and
+ * https://us-central1-smarthome-3c6b9.cloudfunctions.net/updateDeviceStatus?id=my9iXu6WvEgx5oNLLegs&enabled=false
+ * to confirm that the device is turned off
+ * @type {HttpsFunction}
+ */
+exports.updateDeviceStatus = functions.https.onRequest((req, res) => {
+    return cors(req, res, () => {
+        if (req.method !== 'POST') {
+            return res.status(401).json({
+                message: 'Invalid operation'
+            });
+        }
+        updateDeviceStatus(res, req);
+    }) ;
+});
+
+
+const getDeviceFromDB = (res, req) => {
+
+    var docRef = database.collection("Devices").doc(req.query.id);
+
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            return res.status(200).json({
+                [req.query.id] : doc.data()
+            });
+        } else {
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+};
+
+/**
+ * Send a GET request such as
+ * https://us-central1-smarthome-3c6b9.cloudfunctions.net/getDeviceFromDB?id=my9iXu6WvEgx5oNLLegs
+ * @type {HttpsFunction}
+ */
+exports.getDeviceFromDB = functions.https.onRequest((req, res) => {
+    return cors(req, res, () => {
+        if (req.method !== 'GET') {
+            return res.status(401).json({
+                message: 'Invalid operation'
+            });
+        }
+        getDeviceFromDB(res, req);
+    }) ;
 });
