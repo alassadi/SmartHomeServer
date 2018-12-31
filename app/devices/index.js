3db6461268c565682c839d5cead3d7f2ab042406
Federica <federica.comuni@gmail.com>
2018-12-31 18:41:17 +0100

    More refactoring + user authentication

--- app/devices/index.js
+++ app/devices/index.js
@@ -7,52 +7,12 @@ const cors = require('cors')({
 
 const admin = require('firebase-admin');
 
-const database = admin.firestore();
 const realTimeDatabase = admin.database();
 const dbref = realTimeDatabase.ref();
 
-const turnOnDevice = (res, req) => {
-    return database.collection('Devices').doc(req.query.id).update({ 'enabled': true }).then(res.status(200).json({
-        // here we shd get confirmation from the gateway first, but this is for testing purposes
-        [req.query.id]: 'Device is On'
-    })).catch(err => {
-        console.log('Error getting documents', err);
-    });
-};
 
-module.exports.turnOnDevice = functions.https.onRequest((req, res) => {
-    return cors(req, res, () => {
-        if (req.method !== 'GET') {
-            return res.status(401).json({
-                message: 'Not allowed'
-            });
-        }
-        turnOnDevice(res, req);
-    });
-});
-
-const turnOffDevice = (res, req) => {
-    return database.collection('Devices').doc(req.query.id).update({ 'enabled': false }).then(res.status(200).json({
-        //we shd check if the key exists before sending the res
-        // here we shd get confirmation from the gateway first, but this is for testing purposes
-        [req.query.id]: 'Device is Off'
-    })).catch(err => {
-        console.log('Error getting documents', err);
-    });
-};
-
-module.exports.turnOffDevice = functions.https.onRequest((req, res) => {
-    return cors(req, res, () => {
-        if (req.method !== 'GET') {
-            return res.status(401).json({
-                message: 'Not allowed'
-            });
-        }
-        turnOffDevice(res, req);
-    });
-});
-
-/** Send a PUT request such as
+/** With JSON:
+ * Send a PUT request such as
  * https://europe-west1-smarthome-3c6b9.cloudfunctions.net/device
  * with JSON:
  * { "id": "my9iXu6WvEgx5oNLLegs", "enabled": true }
@@ -66,38 +26,22 @@ module.exports.turnOffDevice = functions.https.onRequest((req, res) => {
  * with JSON:
  * { "id": "my9iXu6WvEgx5oNLLegs"}
  * to read device status
- * @type {HttpsFunction}
- */
-module.exports.device = functions.region('europe-west1').https.onRequest((req, res) => {
-    return cors(req, res, () => {
-        if (req.method === 'POST') {
-            var devicesRef = dbref.child('Devices');
-            devicesRef.on('value', function (snapshot) {
-                return res.status(200).json(snapshot.child(req.body.id).val());
-            });
-        } else if (req.method === 'PUT') {
-            return dbref.child('Devices/' + req.body.id).update({ 'enabled': req.body.enabled }).then(res.status(200).json({
-                'enabled': req.body.enabled
-            })).catch(err => {
-                console.log('Error updating database', err);
-            });
-        }
-    });
-});
-
-/** Send a POST request such as
+ *
+ * With URL parameters:
+ * Send a POST request such as
  * https://europe-west1-smarthome-3c6b9.cloudfunctions.net/device_url?id=my9iXu6WvEgx5oNLLegs&enabled=true
  * to turn device on, and
  * https://europe-west1-smarthome-3c6b9.cloudfunctions.net/device_url?id=my9iXu6WvEgx5oNLLegs&enabled=false
  * to turn device off
- * @type {HttpsFunction}
  * Send a GET request such as
  * https://europe-west1-smarthome-3c6b9.cloudfunctions.net/device_url?id=my9iXu6WvEgx5oNLLegs
  * to read device status
  * @type {HttpsFunction}
  */
-module.exports.device_url = functions.region('europe-west1').https.onRequest((req, res) => {
-    return cors(req, res, () => {
+module.exports.device = functions.region('europe-west1').https.onRequest((req, res) => {
+
+    //if (module.exports.authentication(req, res)) {
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
         if (req.method === 'GET') {
             var devicesRef = dbref.child('Devices');
             devicesRef.on('value', function (snapshot) {
@@ -110,8 +54,22 @@ module.exports.device_url = functions.region('europe-west1').https.onRequest((re
                 console.log('Error updating database', err);
             });
         }
-    });
-});
+    } else {
+        if (req.method === 'POST') {
+            var devicesRef = dbref.child('Devices');
+            devicesRef.on('value', function (snapshot) {
+                return res.status(200).json(snapshot.child(req.body.id).val());
+            });
+        } else if (req.method === 'PUT') {
+            return dbref.child('Devices/' + req.body.id).update({'enabled': req.body.enabled}).then(res.status(200).json({
+                'enabled': req.body.enabled
+            })).catch(err => {
+                console.log('Error updating database', err);
+            });
+        }
+        //}
+    }});
+
 
 
 /**
@@ -122,3 +80,26 @@ module.exports.device_url = functions.region('europe-west1').https.onRequest((re
 module.exports.onDeviceUpdated = functions.region('europe-west1').database.ref('Devices').onUpdate((snapshot, context) => {
     console.log('the status of devices on realtime db has been updated');
 });
+
+/**
+ * Checks user authentication. ID token
+ * needs to be fetched on the front end after
+ * the user has been created
+ * @param req
+ * @param res
+ */
+module.exports.authentication = function authentication(req, res) {
+    cors(req, res, () => {
+        const tokenId = req.get('Authorization').split('Bearer')[1];
+
+        return admin.auth().verifyIdToken(tokenId)
+            .then((decoded) =>  {res.status(200).send(decoded)
+                console.log("Authorized request")
+                return true
+            })
+            .catch((err) => {res.status(401).send(err)
+                console.log("Unauthorized request")
+                return false
+            });
+    });
+};
