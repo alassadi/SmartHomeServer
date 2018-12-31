eea3ffc24d569e669c1ccb92335b3de0445d2607
Martin H <martinh@Patrick.local>
2018-12-31 14:52:33 +0000

    Refactored user and unit endpoints
    Endpoints are now more restful
    - requires testing

--- app/units/index.js
+++ app/units/index.js
@@ -1,42 +1,120 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const cors = require('cors')({
+    origin: true
+});
 
-exports.registerUnit = functions.https.onRequest((req, res) => {
-    const dbref = admin.firebase().ref();
-    if(req.method !== "POST") {
-        return res.status(400).json({
-            message: "Bad request, POST Required"
-        });
-    }
-    const data = req.body;
-    dbref.child('Users').child(data.uid).child('Units').push({
-        unit_type : data.unit_type.toString(),
-        fcm_token : data.fcm_token.toString()
-    }).then((unit) => {
-        return res.status(200).json({
-            message: 'Device registered successfully',
-            uid: unit.key
-        })
-    }).catch((error) => {
-        res.json({
-            message: "error" + error.toString()
-        })
+exports.unit = functions.region('europe-west1').https.onRequest((req, res) => {
+    return cors(res, req, () => {
+        const dbref = admin.database.ref();
+
+        let unit_uid = '';
+        if (req.method === 'PUT' || req.method === 'GET') {
+            if (req.query.hasOwnProperty('id')) {
+                unit_uid = req.query.id;
+            } else if (req.body.hasOwnProperty('id')) {
+                unit_uid = req.body.id
+            } else {
+                return res.status(400).json({
+                    message: "No Unit ID is available in JSON body or URL"
+                });
+            }
+        }
+        if (req.method === 'POST') {
+            const data = req.body;
+            dbref.child('Users').child(data.user_uid).child('Units').push({
+                unit_type : data.unit_type.toString(),
+                fcm_token : data.fcm_token.toString()
+            }).then((unit) => {
+                return res.status(200).json({
+                    message: 'Unit registered successfully',
+                    uid: unit.key
+                })
+            }).catch((error) => {
+                res.json({
+                    message: "error" + error.toString()
+                })
+            })
+        }
+        else if (req.method === 'PUT') {
+            return res.status(401).json({
+                message: 'PUT Denied, please use updateFcmToken endpoint to update this unit\'s fcm_token'
+            })
+        }
+        else if (req.method === 'GET') {
+            let unit = dbref.child('Users').child(data.user_uid).child('Units');
+            unit.once('value').then((snapshot) => {
+                if(!snapshot.child(unit_uid).exists()) {
+                    return res.status(404).json({
+                        message: 'Unit with specified UID does not exist.'
+                    })
+                }
+            });
+            if (unit_uid === '') {
+                return res.status(400).json({
+                    message: 'Denied. Uid must be present either as a URL parameter or as part of the request body.'
+                })
+            }
+            unit.on('value', function (snapshot) {
+                return res.status(200).json(
+                    snapshot.child(unit_uid).val()
+                );
+            })
+        }
     })
+});
+
+exports.units = functions.https.onRequest((req, res) => {
+    return cors(req, res, () => {
+        const dbref = admin.database.ref();
+        if (req.method !== 'GET') {
+            return res.status(401).json({
+                message: 'Denied. Units endpoint is read-only.'
+            })
+        }
+
+        let user_uid = '';
+        if (req.query.hasOwnProperty('user_uid')) {
+            user_uid = req.query.id;
+        } else if (req.body.hasOwnProperty('user_uid')) {
+            user_uid = req.body.user_uid
+        } else {
+            return res.status(400).json({
+                message: "No User UID is available in JSON body or URL"
+            });
+        }
+
+        let allUnits = dbref.child('Users').child(user_uid).child('Units');
+
+        allUnits.once('value').then((snapshot) => {
+            if(!snapshot.exists()) {
+                return res.status(404).json({
+                    message: 'User has no units.'
+                })
+            }
+        });
+
+        allUnits.on('value', function (snapshot) {
+            return res.status(200).json(
+                snapshot.val()
+            );
+        })
+    });
 });
 
 exports.updateFcmToken = functions.https.onRequest((req, res) => {
     const dbref = admin.firebase().ref();
-    if(req.method !== "POST") {
+    if(req.method !== "PUT") {
         return res.status(400).json({
-            message: "Bad request, POST Required"
+            message: "Bad request, PUT Required"
         })
     }
     const data = req.body;
-    dbref.child('Users').child(data.uid).child('Units').child(data.unit_uid).update({
+    dbref.child('Users').child(data.user_uid).child('Units').child(data.unit_uid).update({
         fcm_token : data.fcm_token.toString()
     })
         .then(() => {
-            console.log("FCM Token added to client for user: " + data.uid)
+            console.log("FCM Token added to client for user: " + data.user_uid);
             return res.status(200).json({
                 message: "Fcm token registered"
             })
